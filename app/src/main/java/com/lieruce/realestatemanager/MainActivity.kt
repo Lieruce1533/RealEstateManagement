42b70f87d44f21bfe20c05d4e8fb506be1ff5cfb
Lieruce1540 <flint.fabien@laposte.net>
2026-08-09 16:09:30 +0200

    Clean up old Java/XML code and set up basic Jetpack Compose theme and MainActivity

---
+++ app/src/main/java/com/lieruce/realestatemanager/MainActivity.kt
@@ -0,0 +1,38 @@
+package com.lieruce.realestatemanager
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import com.lieruce.realestatemanager.ui.theme.RealEstateManagerTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            RealEstateManagerTheme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    Greeting(
+                        name = "Real Estate Agent",
+                        modifier = Modifier.padding(innerPadding)
+                    )
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
